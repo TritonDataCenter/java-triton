4a78a691cb406f815def05255a4f72c9da900aa0
Elijah Zupancic <elijahz@snapskout.com>
2016-04-22 14:29:25 -0700

    Fixed deprecation warnings.

--- src/test/java/com/joyent/triton/FakeHttpClient.java
+++ src/test/java/com/joyent/triton/FakeHttpClient.java
@@ -7,8 +7,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 
 import java.io.IOException;
@@ -33,12 +31,12 @@ public class FakeHttpClient implements HttpClient {
     }
 
     @Override
-    public HttpParams getParams() {
+    public org.apache.http.params.HttpParams getParams() {
         throw new UnsupportedOperationException("We don't test deprecated methods");
     }
 
     @Override
-    public ClientConnectionManager getConnectionManager() {
+    public org.apache.http.conn.ClientConnectionManager getConnectionManager() {
         throw new UnsupportedOperationException("We don't test deprecated methods");
     }
 
